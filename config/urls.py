a0a3af9398c1e025d549eac11efd2f32dc9dabf6
erfan <erfan.arefmehr@gmail.com>
2023-04-03 20:49:03 +0430

    include blog urls as root urls

--- config/urls.py
+++ config/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('account.urls')),
+    path('', include('blog.urls')),
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
     path('account/', include('django.contrib.auth.urls'))
